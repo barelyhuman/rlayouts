c8da4520643bc2fd110fecbb41d6637e2815434f
barelyhuman <barelyhuman@users.noreply.github.com>
2021-02-04 01:46:26 +0000

    ci: formatted

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import { promises as fs } from 'fs';
-import path from 'path';
+import { promises as fs } from 'fs'
+import path from 'path'
 
-export async function moduleOne() {
+export async function moduleOne () {
   try {
-    const content = await fs.readFile(path.join(__dirname,'/index.js'));
-    console.log(content.toString());
+    const content = await fs.readFile(path.join(__dirname, '/index.js'))
+    console.log(content.toString())
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
 }
 
-if(require.main.filename === __filename){
-  moduleOne(); 
+if (require.main.filename === __filename) {
+  moduleOne()
 }
