11bf397b4e8b91c8fa049d7c9ed566212a73a8ae
Reaper Gelera <sidgroks@gmail.com>
2021-02-14 04:10:55 +0530

    ci: simplify log generation

---
+++ generate-changelog.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+version=v0.0.4-dev.0
+curl -s https://api.github.com/repos/barelyhuman/commitlog/releases/tags/$version | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
+tar -xvzf commitlog-linux-amd64.tar.gz
+chmod +x commitlog 
+./commitlog -i fix,feat,refactor > CHANGELOG.txt
+
